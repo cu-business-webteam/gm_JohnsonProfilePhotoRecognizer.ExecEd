be098a03df486aca48f95c75200960a07dc3b7f0
uniblab <tjb284@cornell.edu>
2018-08-07 15:14:19 -0400

    Initial seed
    Signed-off-by: uniblab <tjb284@cornell.edu>

---
+++ src/ExecEdRecognizer.cs
@@ -0,0 +1,15 @@
+﻿namespace Johnson.ProfilePhotoRecognizer.ExecEd {
+
+	public sealed class ExecEdRecognizer : IFileRecognizer {
+
+		public ExecEdRecognizer() : base() {
+		}
+
+
+		public System.String IsRecognized( System.String fileSpecification ) {
+			throw new System.NotImplementedException();
+		}
+
+	}
+
+}
